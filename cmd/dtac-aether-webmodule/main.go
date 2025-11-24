69dd7bba94e6434351c90c8e8a1736dc8efd4081
Ben Grewell <bgrewell@gmail.com>
2025-11-24 07:55:52 -0800

    misc: add initial files

--- cmd/dtac-aether-webmodule/main.go
+++ cmd/dtac-aether-webmodule/main.go
@@ -18,7 +18,6 @@ func main() {
 	mux := http.NewServeMux()
 
 	// Register HTTP routes for this module.
-	wb := dtac_aether_webmodule.NewAetherWebModule()
 	dtac_aether_webmodule.RegisterRoutes(mux)
 
 	log.Printf("Starting dtac-aether-webmodule web module on %s", addr)
